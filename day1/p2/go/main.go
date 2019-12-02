0fcad1dc29c4f50dc00c6619150c0920b7109c4f
Anuar Serdaliyev <serdaliyev.anuar@gmail.com>
2019-12-02 19:48:12 +0100

    day1 p2

--- day1/p2/go/main.go
+++ day1/p2/go/main.go
@@ -3,10 +3,32 @@ package main
 import (
 	"bufio"
 	"os"
+	"strconv"
 )
 
 func main() {
+	var fuelSum int
 	f, _ := os.Open("../input.txt")
-	scanner, _ := bufio.NewScanner(f)
+	scanner := bufio.NewScanner(f)
 
+	for scanner.Scan() {
+		mass, _ := strconv.Atoi(scanner.Text())
+		fuel := mass/3 - 2
+
+		for fuel > 0 {
+			mass = fuel
+			fuelSum += fuel
+		}
+		fuelSum += fuel
+	}
+
+	fmt.Println(fuelSum)
+}
+
+func calcFuel(mass int) {
+	fuel := mass/3 - 2
+	if fuel > 0 {
+		+calcFuel(fuel)
+	}
+	return
 }
