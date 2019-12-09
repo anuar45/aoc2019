119fc19c1cae3bb812aadd7a5e16ff5b0d0ea216
Anuar Serdaliyev <serdaliyev.anuar@gmail.com>
2019-12-09 10:29:24 +0100

    day8 p1 done

--- day8/p1/main.go
+++ day8/p1/main.go
@@ -1,25 +1,63 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
+	"strconv"
 )
 
-type Layer [6][24]string
-
-type Layers []Layer
+type Layer [6][25]int
 
 func main() {
 	input, _ := ioutil.ReadFile("../input.txt")
+	data := []rune(string(input))
 
-	layers := []Layer{}
-
-	for i, v := range string(input) {
+	//fmt.Println(len(data))
+	var layers []Layer
+	var i int
+	//fmt.Printf(" %d-%c ", i, v)
+	for i < len(data) {
 		var layer Layer
-
+		//fmt.Println(len(layer), len(layer[0]))
 		for j := 0; j < len(layer); j++ {
-
+			for k := 0; k < len(layer[j]); k++ {
+				layer[j][k], _ = strconv.Atoi(string(data[i]))
+				i++
+			}
 		}
-
+		layers = append(layers, layer)
 	}
+
+	fmt.Println(layers)
+
+	minZeros := 1<<63 - 1
+	var countZeros, countOnes, countTwos int
+	var result Layer
+	for _, layer := range layers {
+		countZeros = 99999
+		countZeros = countLayerDigits(layer, 0)
+		if countZeros < minZeros {
+			minZeros = countZeros
+			//fmt.Println(countZeros)
+			countOnes = countLayerDigits(layer, 1)
+			countTwos = countLayerDigits(layer, 2)
+			result = layer
+		}
+	}
+
+	fmt.Println(result, minZeros)
+	fmt.Println(countOnes * countTwos)
+}
+
+func countLayerDigits(l Layer, digit int) int {
+	var count int
+	for _, row := range l {
+		for _, v := range row {
+			if v == digit {
+				count++
+			}
+		}
+	}
+	return count
 
 }
