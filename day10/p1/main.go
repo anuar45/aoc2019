4d7b591e0de7669a2728ba542cb32b37b1c61669
Anuar Serdaliyev <serdaliyev.anuar@gmail.com>
2019-12-12 07:18:41 +0100

    day 10 p1 in progress

--- day10/p1/main.go
+++ day10/p1/main.go
@@ -6,29 +6,74 @@ import (
 	"os"
 )
 
+type Point struct {
+	X   int
+	Y   int
+	Val int
+	See []Point
+}
+
+type Points struct {
+	SizeX  int
+	SizeY  int
+	Points []Point
+}
+
 func main() {
 	f, _ := os.Open("../input.txt")
 	scanner := bufio.NewScanner(f)
 
 	scanner.Split(bufio.ScanLines)
 
-	var m [][]int
+	var points []Point
+	var x, y int
 	for scanner.Scan() {
-		var row []int
+		x = 0
 		for _, v := range scanner.Text() {
+			point := Point{X: x, Y: y}
 			if v == '#' {
-				row = append(row, 1)
-			} else {
-				row = append(row, 0)
+				point.Val = 1
 			}
+			points = append(points, point)
+			x++
 		}
-		m = append(m, row)
+		y++
 	}
 
-	for x := 0; x < len(m); x++ {
-		for y := 0; y < len(m); y++ {
-			m[x][y] = m[y][x]
+	pMap := Points{SizeX: x, SizeY: y, Points: points}
+
+	fmt.Println(pMap)
+	print(pMap)
+
+	// for y1 := 0; y1 < len(m); y1++ {
+	// 	for x1 := 0; x1 < len(m[y1]); x1++ {
+	// 		if m[y1][x1] == 1 {
+	// 			for y2 := 0; y2 < len(m); y2++ {
+	// 				for x2 := 0; x2 < len(m[y2]); x2++ {
+	// 					if m[y2][x2] == 1 && y2 != y1 && x2 != x1 {
+
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// }
+
+}
+
+func print(points Points) {
+	for i, point := range points.Points {
+		fmt.Print(point.Val)
+		if i == points.SizeX {
+			fmt.Println()
 		}
 	}
-	fmt.Println(m[0][2], m[1][2], m[2][2], m[3][2], m[4][2])
+}
+
+func findSlope(x1, y1, x2, y2 int) int {
+	var result int
+	yDiff := y2 - y1
+	xDiff := x2 - x1
+	result = yDiff / xDiff
+	return result
 }
